c490ec084f5c87ed0e92adb0ab088b3628861263
Rafael Segura <rafael.segura.orta@gmail.com>
2025-12-04 23:26:57 +0100

    Commit room

---
+++ app/src/main/java/com/proyecto/recordnote/domain/model/CategoriaDomain.kt
@@ -0,0 +1,4 @@
+package com.proyecto.recordnote.domain.model
+
+class CategoriaDomain {
+}
---
+++ app/src/main/java/com/proyecto/recordnote/presentation/viewmodel/InicioViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.proyecto.recordnote.presentation.viewmodel
+
+class InicioViewModelFactory {
+}
